376ae806d2ea8362c9dece393ab2ea6820023241
christoforadiana <christoforadianayuliawati@gmail.com>
2023-11-04 17:48:06 +0700

    pull

--- server/routes/destination.js
+++ server/routes/destination.js
@@ -1,13 +1,17 @@
-const destinyRoute = require('express').Router()
-const DestinationController = require('../controllers/DestinationController')
-const { authentication } = require('../middleware/authorization')
+const destinyRoute = require("express").Router();
+const DestinationController = require("../controllers/DestinationController");
+const { authentication } = require("../middleware/authorization");
 
-destinyRoute.get('/', DestinationController.getListDestination)
-destinyRoute.get('/information/:id', DestinationController.destinyInformation)
-destinyRoute.post('/add', DestinationController.addDestination)
-destinyRoute.patch('/edit/:id', DestinationController.editDestination)
-destinyRoute.delete('/remove/:id', DestinationController.remove)
-destinyRoute.get('/recomendation', DestinationController.recomendation)
-destinyRoute.post('/review/:id', authentication, DestinationController.rateDestination)
+destinyRoute.get("/", DestinationController.getListDestination);
+destinyRoute.get("/information/:id", DestinationController.destinyInformation);
+destinyRoute.post("/add", DestinationController.addDestination);
+destinyRoute.patch("/edit/:id", DestinationController.editDestination);
+destinyRoute.delete("/remove/:id", DestinationController.remove);
+destinyRoute.get("/recomendation", DestinationController.recomendation);
+destinyRoute.post(
+  "/review/:id",
+  authentication,
+  DestinationController.rateDestination
+);
 
-module.exports = destinyRoute
+module.exports = destinyRoute;
