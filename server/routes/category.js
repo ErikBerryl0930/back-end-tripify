1fc510c780040309fda7e3b6f52cb36eff6d0365
nisfu-saaban <nisthy07@gmail.com>
2023-11-05 20:25:41 +0700

    add admin in routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,10 +1,11 @@
 const categoryRoute = require("express").Router();
 const CategoryController = require("../controllers/CategoryController");
+const { authentication, isAdmin } = require('../middleware/authorization')
 
 
 categoryRoute.get("/", CategoryController.getListCategory);
-categoryRoute.post("/add", CategoryController.addCategory);
-categoryRoute.patch("/edit/:id", CategoryController.editCategory);
-categoryRoute.delete("/remove/:id", CategoryController.deleteCategory);
+categoryRoute.post("/add", authentication, isAdmin, CategoryController.addCategory);
+categoryRoute.patch("/edit/:id", authentication, isAdmin, CategoryController.editCategory);
+categoryRoute.delete("/remove/:id", authentication, isAdmin, CategoryController.deleteCategory);
 
 module.exports = categoryRoute;
