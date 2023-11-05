11d07352221f530a671604d6708e09b49fe364f2
nisfu-saaban <nisthy07@gmail.com>
2023-11-05 12:03:24 +0700

    adding transaction

--- server/controllers/TransactionController.js
+++ server/controllers/TransactionController.js
@@ -11,8 +11,38 @@ class TransactionController {
     }
   }
 
-  static async addTransaction(req, res) {
-  }
+  static addTransaction = async (req, res) => {
+
+    const {
+      date_checkin,
+      qty_ticket,
+      total_price,
+      transaction_type,
+      transaction_detail,
+      invoice_number,
+      status,
+      userId,
+      destinationId,
+    } = req.body;
+
+    try {
+      let transaction = await transaction.create({
+        date_checkin,
+        qty_ticket,
+        total_price,
+        transaction_type,
+        transaction_detail,
+        invoice_number,
+        status,
+        userId,
+        destinationId,
+      });
+
+      res.status(201).json({ message: 'Transaction has been added' });
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
+  };
 
 }
 
