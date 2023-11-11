f5bd35aab3f00911a717985833e04c7fee668d57
christoforadiana <christoforadianayuliawati@gmail.com>
2023-11-12 03:18:06 +0700

    login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,45 +1,143 @@
-import { useState,useEffect } from "react"
-import {useNavigate} from "react-router-dom"
-import {useDispatch,useSelector} from "react-redux"
-import {auth} from "../../api/login.axios"
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../../api/login.axios";
 
-import "./login.scss"
+const Copyright = (props) => {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Tripify
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
 
 const Login = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-const [form, setForm] = useState({
-  email : "",
-  password : ""
-})
+  const { isLogin, loading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-const { isLogin, loading} = useSelector((state) => state.auth)
-const dispatch = useDispatch()
-const navigate = useNavigate()
-
-const login = (e) => {
-  e.preventDefault()
-  dispatch(auth(form))
-}
+  const login = (e) => {
+    e.preventDefault();
+    dispatch(auth(form));
+  };
 
   useEffect(() => {
-    if(isLogin && localStorage.getItem("token")){
-      navigate('/')
+    if (isLogin && localStorage.getItem("token")) {
+      navigate("/");
     }
-  },[isLogin, navigate])
+  }, [isLogin, navigate]);
+
+  const defaultTheme = createTheme();
 
   return (
-    <div>
-      <div>
-      <label>email</label>
-      <input onChange={(e) => setForm({...form, email: e.target.value})} type="email" placeholder="email" />
-      </div>
-      <div>
-      <label>password</label>
-      <input onChange={(e) => setForm({...form, password: e.target.value})} type="password" placeholder="password" />
-      </div>
-      <button onClick={login}>Login</button>
-    </div>
-  )
-}
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={login} sx={{ mt: 1 }}>
+              <TextField
+                onChange={(e) => setForm({ ...form, email: e.target.value })}
+                // type="email"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                onChange={(e) => setForm({ ...form, password: e.target.value })}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+              </Grid>
+              <Copyright sx={{ mt: 5 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
+};
 
-export default Login
+export default Login;
